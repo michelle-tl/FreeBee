2a0fbdf340814e3933da96aeb0667684e25ef32e
hjorthjort <rikard.hjort@gmail.com>
2019-05-10 10:06:39 +0200

    Get a decently sized list of stations

--- server/Stations.js
+++ server/Stations.js
@@ -9,17 +9,26 @@ const fs        = require('fs');
 // Sort out very small cities.
 const CITY_MIN_POPULATION = 50000;
 
+// A list of all country codes in Europe, e.g., SE for Sweden.
 const europeanCountryCodes =
     Object.entries(countries.countries)
       .filter(([k, v]) => v.continent == 'EU')
       .map(([k, _]) => k);
 
+// A list of all cities in Europe (with some extra data). Cities with small
+// populations are filtered out.
 const europeanCities =
     cities
       .filter(c => europeanCountryCodes.indexOf(c.country) != -1)
       .filter(c => c.population >= CITY_MIN_POPULATION)
-      .map(c => ({'name' : c.name, 'countryName': countries.countries[c.country].name} ));
+      .map(c => {
+          c['countryName'] = countries.countries[c.country].name;
+          return c;
+      });
 
+// For every city, do a best-effort attempt at getting a train station from
+// interrail for it. Since this is a text-based search, in some cases we will
+// end up with cities missing, or incorrectly labeled.
 const queries = europeanCities.map((city) =>
                                    (callback) =>
            interrail.stations.search(city.name + " " + city.countryName, {results: 1})

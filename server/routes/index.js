de8d1f2a2c15b09f432baaaef19590cc647f004a
Felix Nordén <felixnorden@gmail.com>
2019-05-11 11:30:46 +0200

    Add routes for initiate and iterate operations

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-   res.status(200).send('Hello World');
+// const graph = require('../graph.js');
+
+router.get('/', function(req, res) {
+  res
+    .status(200)
+    .send('Incorrect endpoint, please use either /initiate or /iterate!');
+});
+router.get('/initiate', (req, res) => {
+  const { from, to } = req.query;
+  if (from && to) {
+    res.json({
+      graph: {},
+      plans: [
+        {
+          place: 'Stockholm',
+          travelMinutes: '120',
+          stayMinutes: '60'
+        },
+        {
+          place: 'Halsberg',
+          travelMinutes: '100',
+          stayMinutes: '30'
+        },
+        {
+          place: 'Gothenburg',
+          travelMinutes: '150',
+          stayMinutes: '40'
+        }
+      ]
+    });
+  } else {
+    res.json({ error: "Incorrect parameters, need 'from' and 'to'" });
+  }
+});
+
+router.post('/iterate', (req, res) => {
+  // const { path, acoGraph, preferences } = req.body;
+  console.log(req.body);
+  // res.json(req.body);
 });
 
 module.exports = router;

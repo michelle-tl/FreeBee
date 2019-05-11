4ca04f588113f08df9d228be79ae799b9bad8fcc
bexi <trreitmaier@msn.com>
2019-05-11 12:22:22 +0200

    cors is now enabled

--- client/src/PlansPage.js
+++ client/src/PlansPage.js
@@ -2,36 +2,43 @@ import React, { useState, useEffect }  from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import axios from 'axios';
-
+axios.defaults.withCredentials = true;
 
 const Page_2 = () => {
   // Declare a new state variable, which we'll call "count"
-  const [plans, setPlans] = useState([]);
+  const [plans, setPlans] = useState();
   const [selectedPlan, setSelectedPlan] = useState(undefined);
 
-  // Similar to componentDidMount and componentDidUpdate:
-   useEffect(() => {
-     axios.get(`http://localhost:5000/`)
-           .then(res => {
-               const res_plans = res.data.plans;
-               setPlans({ res_plans });
-               console.log(res_plans);
-           })
-       });
 
+   useEffect(() => {
+       const fetchData = async () => {
+         const res = await axios.get(
+             `http://localhost:5000/initiate`,
+         );
+
+         const res_plans = res.data.plans;
+         setPlans({ res_plans });
+     };
+       fetchData();
+     }, []);
+
+console.log(plans);
   return (
-      <Container>
-  <Row>
-    <Col>1 of 2</Col>
-    <Col>2 of 2</Col>
-  </Row>
-  <Row>
-    <Col>1 of 3</Col>
-    <Col>2 of 3</Col>
-    <Col>3 of 3</Col>
-  </Row>
-</Container>
+      <div>{plans}</div>
   )
+  /*
+  <Container>
+<Row>
+<Col>1 of 2</Col>
+<Col>2 of 2</Col>
+</Row>
+<Row>
+<Col>1 of 3</Col>
+<Col>2 of 3</Col>
+<Col>3 of 3</Col>
+</Row>
+</Container>
+*/
 }
 
 /*

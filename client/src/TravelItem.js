06c5fc5c18064977bd1a0ca0c9bb587a5d3b2010
bexi <trreitmaier@msn.com>
2019-05-11 14:44:43 +0200

    created component for a travel item

---
+++ client/src/TravelItem.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect }  from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const TravelItem = (props) => {
+
+  return (
+    <Row>
+    <div className='travel-item'>
+    this is a travel item
+    </div>
+    </Row>
+  )
+}
+
+export default TravelItem;

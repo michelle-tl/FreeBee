dca71a0bec9de0c773f96796d82df27e87dacc38
bexi <trreitmaier@msn.com>
2019-05-11 16:47:09 +0200

    updating travel plans should work, backend needs fixing?

--- client/src/PlansPage.js
+++ client/src/PlansPage.js
@@ -5,34 +5,126 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './Plans.css';
 import SearchQueries from './SearchQueries.js';
 import TravelItem from './TravelItem'
-
 import axios from 'axios';
-//axios.defaults.withCredentials = true;
-
 
 const Page_2 = (props) => {
   // Declare a new state variable, which we'll call "count"
   const [plans, setPlans] = useState();
   const [selectedPlan, setSelectedPlan] = useState(undefined);
+  const [graph, setGraph] = useState(undefined);
 
+
+  const TitleComponent = () =>{
+    return(
+      <div class="title">
+          <h1 class="primar-color">Some awsome travel plans.</h1>
+          <h4>Are you ready for your adventure?!</h4>
+      </div>
+    )
+  }
+
+  const NewSearchComponent = () =>{
+      return(
+      <div class="NewSearch">
+          <div className="">Didn't find a journey of your dreams?</div>
+          <div className=''>No worries!</div>
+          <Button className='button-primary-color'>Give me more!</Button>
+      </div>
+      )
+  }
+
+  // handler for button -- fetches new plans
+  const handleIteration = (e, plan) => {
+      console.log('plan');
+      console.log(plan);
+      console.log('graph');
+      console.log(graph);
+
+      const fetchNewPlans = async () => {
+
+        const initUrl = `http://localhost:5000/iterate`;
+        const data = {graph: graph.res_graph, plan: plan};
+        const res = await axios.post(initUrl, data)
+
+         const res_plans = res.data.plans;
+         const res_graph = res.data.graph;
+         setPlans({res_plans});
+         setGraph({res_graph});
+    };
+      fetchNewPlans();
+  }
+
+  // the travel suggestions
+  // TODO: add information about each travel
+  const SuggestionsComponent = (props) => {
+
+      if (props.plans) {
+          var plan_0 = props.plans.res_plans[0];
+          var plan_1 = props.plans.res_plans[1];
+          var plan_2 = props.plans.res_plans[2];
+
+          const items_0 = []
+          for (const [index, value] of plan_0.entries()) {
+            items_0.push(<li key={index}>{value.place}</li>)
+          }
+          const items_1 = []
+          for (const [index, value] of plan_1.entries()) {
+            items_1.push(<li key={index}>{value.place}</li>)
+          }
+          const items_2 = []
+          for (const [index, value] of plan_2.entries()) {
+            items_2.push(<li key={index}>{value.place}</li>)
+          }
+
+          return (
+              <div class="Suggestions">
+              <Row>
+                <div class="Sug-Title">So this is your cup of tea? </div>
+                <div class="Sug-Title">We have more that came from! </div>
+              </Row>
+
+              <div class="Sugg-List">
+
+                    <Row>
+                    <Col>
+                      <Button size="sm" onClick={(e) => handleIteration(e, plan_0)}>Explore similar</Button>
+                      <div><ul>{items_0}</ul></div>
+                    </Col>
+                    <Col>
+                      <Button size="sm">Explore similar</Button>
+                      <div><ul>{items_1}</ul></div>
+                    </Col>
+                    <Col>
+                      <Button size="sm">Explore similar</Button>
+                      <div><ul>{items_2}</ul></div>
+                    </Col>
+                    </Row>
+              </div>
+            </div>
+      );
+      }else{
+          return <div/>
+      }
+  }
+
+// 'component did mount'
+// TODO update to fetch the correct input
    useEffect(() => {
        const fetchData = async () => {
-         /*const res = await axios.get(
-             `http://localhost:5000/initiate`,
-         );*/
+
          const initUrl = `http://localhost:5000/initiate`;
          const data = {from:'LONDON (United Kingdom)', to:'BARCELONA CITY (Spain)'};
          const res = await axios.post(initUrl, data)
 
          const res_plans = res.data.plans;
-         //console.log(res_plans);
+         const res_graph = res.data.graph;
          setPlans({res_plans});
+         setGraph({res_graph});
      };
        fetchData();
      }, []);
 
-// {props.location.state.cityFrom} - {props.location.state.cityTo}
-console.log(plans);
+ // Container for all the plans with buttons etc
   return (
       <div class="plans-container">
         <SearchQueries query={props.location.state}></SearchQueries>
@@ -48,77 +140,6 @@ console.log(plans);
   )
 }
 
-const TitleComponent = () =>{
-  return(
-    <div class="title">
-        <h1 class="primar-color">Some awsome travel plans.</h1>
-        <h4>Are you ready for your adventure?!</h4>
-    </div>
-  )
-}
-
-const NewSearchComponent = () =>{
-    return(
-    <div class="NewSearch">
-        <div className="">Didn't find a journey of your dreams?</div>
-        <div className=''>No worries!</div>
-        <Button className='button-primary-color'>Give me more!</Button>
-    </div>
-    )
-}
-
-const SuggestionsComponent = (props) => {
-
-    console.log(props.plans);
-    if (props.plans) {
-        var plan_0 = props.plans.res_plans[0];
-        var plan_1 = props.plans.res_plans[1];
-        var plan_2 = props.plans.res_plans[2];
-
-        console.log(plan_0);
-        const items_0 = []
-        for (const [index, value] of plan_0.entries()) {
-          items_0.push(<li key={index}>{value.place}</li>)
-        }
-        const items_1 = []
-        for (const [index, value] of plan_1.entries()) {
-          items_1.push(<li key={index}>{value.place}</li>)
-        }
-        const items_2 = []
-        for (const [index, value] of plan_2.entries()) {
-          items_2.push(<li key={index}>{value.place}</li>)
-        }
-
-        return (
-            <div class="Suggestions">
-            <Row>
-              <div class="Sug-Title">So this is your cup of tea? </div>
-              <div class="Sug-Title">We have more that came from! </div>
-            </Row>
-
-            <div class="Sugg-List">
-
-                  <Row>
-                  <Col>
-                    <Button size="sm">Explore similar</Button>
-                    <div><ul>{items_0}</ul></div>
-                  </Col>
-                  <Col>
-                    <Button size="sm">Explore similar</Button>
-                    <div><ul>{items_1}</ul></div>
-                  </Col>
-                  <Col>
-                    <Button size="sm">Explore similar</Button>
-                    <div><ul>{items_2}</ul></div>
-                  </Col>
-                  </Row>
-            </div>
-          </div>
-    );
-    }else{
-        return <div/>
-    }
-}
 
 /*
 

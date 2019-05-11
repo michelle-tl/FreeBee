df312f087742e8b447ec7f1eda2ab91b8afd29de
Michelle Tran Luu <miss_amtl@live.com>
2019-05-11 12:00:02 +0200

    added first ppage

--- client/src/WelcomePage.js
+++ client/src/WelcomePage.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Welcome.css';
+import DatePicker from "react-datepicker";
+ 
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 const Page_1 = () => {
   // Declare a new state variable, which we'll call "count"
   const [cityFrom, setCityFrom] = useState(0);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   function handleStatusChange(event) {
     setCityFrom(event.target.value)
@@ -12,15 +21,65 @@ const Page_1 = () => {
       alert('City From: ' + cityFrom);
       event.preventDefault();
   }
+  function handleStartDateChange(date){
+    setStartDate(date);
+  }
+
+  function handleEndDateChange(date){
+    setEndDate(date);
+  }
 
   return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          From City:
-          <input type="text" value={cityFrom} onChange={handleStatusChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    <div class="welcome-container">
+      <div class="form">
+        <Form>
+        <h2>Explore your next journey.</h2>
+        <Form.Group className="form-input" controlId="formBasicFrom">
+          <Form.Label>From</Form.Label>
+          <Form.Control placeholder="From where?" />
+        </Form.Group>
+
+        <Form.Group className="form-input"controlId="formBasicTo">
+          <Form.Label>To</Form.Label>
+          <Form.Control placeholder="Where to?" />
+        </Form.Group>
+        <div class="form-date">
+        <Form.Group className="form-input" controlId="formBasicBeginning">
+          <Form.Label>Date of Beginning</Form.Label>
+          {/* <Form.Control placeholder="Where to?" /> */}
+          <DatePicker
+            selected={startDate}
+            onChange={handleStartDateChange}
+          />
+        </Form.Group>
+        <Form.Group className="form-input" controlId="formBasicArrival">
+          <Form.Label>Date of Arrival</Form.Label>
+          {/* <Form.Control placeholder="Where to?" /> */}
+          <DatePicker
+            selected={endDate}
+            onChange={handleEndDateChange}
+          />
+        </Form.Group>
+
+        </div>
+        <Form.Group className="form-input" controlId="exampleForm.ControlSelect1">
+          <Form.Label>Number of Travellers</Form.Label>
+          <Form.Control as="select">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6+</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+
+      </Form>
+      </div>
+    </div>
   );
 }
 

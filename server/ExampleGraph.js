b5cd0fc99a7ba4dcb85f1feca5f313907da6da34
hjorthjort <rikard.hjort@gmail.com>
2019-05-11 16:56:09 +0200

    Bugfix ExampleGraph.js

--- server/ExampleGraph.js
+++ server/ExampleGraph.js
@@ -4,11 +4,11 @@ const graphy = require("@dagrejs/graphlib");
 
 const g = new graphy.Graph({directed : false});
 
-g.setEdge("Goteborg", "Stockholm", { minutes: 200});
-g.setEdge("Orebro", "Stockholm", { minutes: 10});
-g.setEdge("Orebro", "Goteborg", { minutes: 1000});
-g.setEdge("Jonkoping", "Stockholm", { minutes: 150});
-g.setEdge("Goteborg", "Jonkoping", { minutes: 70});
+g.setEdge("Goteborg", "Stockholm", 200);
+g.setEdge("Orebro", "Stockholm", 10);
+g.setEdge("Orebro", "Goteborg", 1000);
+g.setEdge("Jonkoping", "Stockholm", 150);
+g.setEdge("Goteborg", "Jonkoping", 70);
 
 
 let suggestions = sugg.initialSuggestions("Goteborg", "Stockholm", g);

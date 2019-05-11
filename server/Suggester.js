f5794aeec3634fdb0ead70e37e6990eb283ea115
Felix Nordén <felixnorden@gmail.com>
2019-05-11 12:38:23 +0200

    Remove log statements

--- server/Suggester.js
+++ server/Suggester.js
@@ -1,44 +1,43 @@
-const graphy = require("@dagrejs/graphlib");
+const graphy = require('@dagrejs/graphlib');
 
 const NUM_SUGGESTIONS = 3;
 
 function dijkstraResToPath(res, from, to) {
-    const path = [];
-    let curr = to;
-    console.log(res);
-    while (curr != from) {
-        let leg = { 'place': curr, 'travelMins': res[curr].distance };
-        path.push(leg);
-        curr = res[curr].predecessor;
-    }
-    let leg = { 'place': from, 'travelMins': 0 };
+  const path = [];
+  let curr = to;
+  while (curr != from) {
+    let leg = { place: curr, travelMins: res[curr].distance };
     path.push(leg);
-    path.reverse();
+    curr = res[curr].predecessor;
+  }
+  let leg = { place: from, travelMins: 0 };
+  path.push(leg);
+  path.reverse();
 
-    // Modify so that every edge carries its weight, not total time taken.
-    let totalTime = 0;
-    path.forEach(leg => {
-        leg.travelMins -= totalTime;
-        totalTime += leg.travelMins;
-    });
+  // Modify so that every edge carries its weight, not total time taken.
+  let totalTime = 0;
+  path.forEach(leg => {
+    leg.travelMins -= totalTime;
+    totalTime += leg.travelMins;
+  });
 
-    return path;
-    // => { A: { distance: 0 },
-    //      B: { distance: 6, predecessor: 'C' },
-    //      C: { distance: 4, predecessor: 'A' },
-    //      D: { distance: 2, predecessor: 'A' },
-    //      E: { distance: 8, predecessor: 'F' },
-    //      F: { distance: 4, predecessor: 'D' } }
+  return path;
+  // => { A: { distance: 0 },
+  //      B: { distance: 6, predecessor: 'C' },
+  //      C: { distance: 4, predecessor: 'A' },
+  //      D: { distance: 2, predecessor: 'A' },
+  //      E: { distance: 8, predecessor: 'F' },
+  //      F: { distance: 4, predecessor: 'D' } }
 }
 
 // Takes path like above, gives edges as length-2 lists.
 function pathToEdges(path) {
-    let edges = [];
-    for (let i = 0; i < path.length - 1; i++) {
-        let edge = [path[i]['place'], path[i+1]['place']];
-        edges.push(edge);
-    }
-    return edges;
+  let edges = [];
+  for (let i = 0; i < path.length - 1; i++) {
+    let edge = [path[i]['place'], path[i + 1]['place']];
+    edges.push(edge);
+  }
+  return edges;
 }
 
 // Produce (at most) NUM_SUGGESTIONS suggested paths, all different.
@@ -54,23 +53,27 @@ function pathToEdges(path) {
 //     { place: 'Stockholm', travelMins: 10 } ]
 // ]
 module.exports.initialSuggestions = (from, to, _travelGraph) => {
-    const travelGraphCopy = graphy.json.read((graphy.json.write(_travelGraph)));
-    const suggestions = [];
-    for (let i = 0; i < NUM_SUGGESTIONS; i++) {
-        const dijkstra = graphy.alg.dijkstra(travelGraphCopy, from, (eId => travelGraphCopy.edge(eId).minutes));
-        let suggestion;
-        try {
-            suggestion = dijkstraResToPath(dijkstra, from, to);
-        } catch (err) {
-            console.error("Failed to generate suggestion");
-            console.error(err);
-            continue;
-        }
-        const edges = pathToEdges(suggestion);
-        edges.forEach(e => travelGraphCopy.removeEdge(e[0], e[1]));
-        suggestions.push(suggestion);
+  const travelGraphCopy = graphy.json.read(graphy.json.write(_travelGraph));
+  const suggestions = [];
+  for (let i = 0; i < NUM_SUGGESTIONS; i++) {
+    const dijkstra = graphy.alg.dijkstra(
+      travelGraphCopy,
+      from,
+      eId => travelGraphCopy.edge(eId).minutes
+    );
+    let suggestion;
+    try {
+      suggestion = dijkstraResToPath(dijkstra, from, to);
+    } catch (err) {
+      console.error('Failed to generate suggestion');
+      console.error(err);
+      continue;
     }
-    return suggestions;
+    const edges = pathToEdges(suggestion);
+    edges.forEach(e => travelGraphCopy.removeEdge(e[0], e[1]));
+    suggestions.push(suggestion);
+  }
+  return suggestions;
 };
 
 
